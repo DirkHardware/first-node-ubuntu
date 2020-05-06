8248b85b7013ec37bf711eb136d0d1a9c02676a5
DirkHardware <email.the.anderson@gmail.com>
2020-05-06 16:38:30 -0400

    chapter 21

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const emitter2 = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg.url);
 });
 
+// Register a logging listener
+emitter2.on('Logging', (arg) => {
+    console.log(`Calm down ${arg} you jackass Im logging`)
+})
+
+emitter2.emit('Logging', "Anderson")
+
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'dicks.com'});
+
+// Raise: logging (data: message
+
