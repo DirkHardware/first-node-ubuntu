118a96659907d1cb8851c5c1388117b8f07195d0
DirkHardware <email.the.anderson@gmail.com>
2020-05-06 16:12:48 -0400

    chapter 20

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const path = require('path')
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-var pathObj = path.parse(__filename);
+// Register a listener
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
+});
 
-console.log(pathObj)
+// Raise an event
+emitter.emit('messageLogged');
