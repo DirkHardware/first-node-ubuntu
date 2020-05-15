edfc14126ef6b0e326ef78147685e91eceabf9fa
DirkHardware <email.the.anderson@gmail.com>
2020-05-15 16:58:47 -0400

    playing around with routing in node

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
+const http = require('http')
 
-// Imports the Logger event class from logger.js, allowing us to create a local instance of that class as logger on line 5
+const server = http.createServer((req, res)=>{
+    if (req.url === '/'){
+        res.write(`Get your yankee ass off my property.`);
+        res.write(`\nI don't care if the bank gave you the deed.`)
+        res.end();
+    };
 
-const Logger = require('./logger');
-const logger = new Logger();
+    if (req.url === '/unknown/hinson'){
+        res.write(JSON.stringify([1, 2, `School? Ain't that the damn place they got all them uhhh, let's see, watcha call 'um, uhh? Fold outs covered in scribblings wrote up all over?`]))
+        res.end()
+    }
+});
 
-let i = 0;
-let notname = 'not the name variable';
+server.on('connection', (socket) => {
+    console.log('new connection')
+});
 
-// Register a listener
-// This structure is counterintuitive but it works as follows:
-// logger.on sends the first and second argument to the logger class in logger.js. The "messageLogging" and "messageLogged" arguments are keywords which,
-// when matched to their counterparts in in the emmitter methods in logger.js, fire their emitters.
+server.listen(3000);
 
-logger.on('messageLogging', (name) => {
-    // console.log here actually prints the second argument in the emit function in the logger.js module. I don't know how it does this, but without it
-    // you won't see the text in the emitter.
-    console.log(name);
-
-    // Curiously, when given a console.log without a corresponding argument in the Logger class' arguments, it will not print the emitter message but
-    // will behave normally, as observed with the i and notname variables
-    i++ 
-    console.log(i);
-    console.log(notname);
-    });
-logger.on('messageLogged', (arg) => {
-    console.log(arg);
-    });
-
-
-// Without this logger.log function though, the console.log functions in our .on methods, they will not have access to the either the fire keywords data 
-// in the logger.js class/instance and NOTHING will fire
-logger.log('Anderson', { id: 1, url: 'website.com'});
+console.log('Listening on Port 3000...')
