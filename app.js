cff36f6f1f45c95b15d1f51e61791471314322bf
anderson <anderson@warmachine.fios-router.home>
2020-06-10 16:45:56 -0400

    seperate file for req/res examples

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-var _ = require('underscore');
+// var _ = require('underscore');
 
-var result = _.contains([1,2,3], 2)
-console.log(result)
-
-// const http = require('http')
-
-// const server = http.createServer((req, res)=>{
-//     if (req.url === '/'){
-//         res.write(`Get your yankee ass off my property.`);
-//         res.write(`\nI don't care if the bank gave you the deed.`)
-//         res.end();
-//     };
-
-//     if (req.url === '/unknown/hinson'){
-//         res.write(JSON.stringify([1, 2, `School? Ain't that the damn place they got all them uhhh, let's see, watcha call 'um, uhh? Fold outs covered in scribblings wrote up all over?`]))
-//         res.end()
-//     }
-// });
-
-// server.on('connection', (socket) => {
-//     console.log('new connection')
-// });
-
-// server.listen(3000);
-
-// console.log('Listening on Port 3000...')
+// var result = _.contains([1,2,3], 2)
+// console.log(result)
 
 // something
---
+++ req-res-example.js
@@ -0,0 +1,23 @@
+
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    if (req.url === '/'){
+        res.write(`Get your yankee ass off my property.`);
+        res.write(`\nI don't care if the bank gave you the deed.`)
+        res.end();
+    };
+
+    if (req.url === '/unknown/hinson'){
+        res.write(JSON.stringify([1, 2, `School? Ain't that the damn place they got all them uhhh, let's see, watcha call 'um, uhh? Fold outs covered in scribblings wrote up all over?`]))
+        res.end()
+    }
+});
+
+server.on('connection', (socket) => {
+    console.log('new connection')
+});
+
+server.listen(3000);
+
+console.log('Listening on Port 3000...')
