0e695852701bd83e27208f13b0b66cddceb9f8b6
anderson <anderson@warmachine.fios-router.home>
2020-06-10 17:19:39 -0400

    now with nodemon

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var _ = require('underscore');
+const express = require('express')
+const server = express();
 
-var result = _.contains([1,2,3], 2)
-console.log(result)
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-something
+server.get('/unknown/hinson', (req, res) => {
+    res.send(JSON.stringify(["Get your yankee ass of my property.\nI don't care if the bank gave you the deed.", 5, true]))
+});
+
+server.get('/api/courses', (req, res) => {
+    res.send([343, 2501, "How to fist fight a bear"])
+});
+
+
+server.listen(3000, () => console.log('Listening on port 3000'))
