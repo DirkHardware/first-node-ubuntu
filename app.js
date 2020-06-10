c03c30e7920d585359554e88b3179ebf6c1e106a
anderson <anderson@warmachine.fios-router.home>
2020-06-10 17:49:09 -0400

    parameters

--- app.js
+++ app.js
@@ -13,5 +13,19 @@ server.get('/api/courses', (req, res) => {
     res.send([343, 2501, "How to fist fight a bear"])
 });
 
+//Node does not suffer from the issue Rails (and I think React Routes?) has where URLs must be listed in order of most complex to most root in order for more complex URLs to not simply direct you to their nearest listed root. 
 
-server.listen(3000, () => console.log('Listening on port 3000'))
+server.get('/api/courses/:id', (req, res) => {
+    res.send(req.params)
+})
+
+server.get('/blog/posts/:id/:year/:month', (req, res) => {
+    //returns query strings. See chapter 47 - Route parameters
+    res.send(req.query)
+})
+
+// PORT is an environmental variable.
+// Ports are assigned dynamically by servers, so we need to set the the port we use with the PORT variable
+//You can set your PORT environment variable manually with export PORT={port number} in terminal
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`))
